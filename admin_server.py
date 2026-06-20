e76edc320e51c82bd999eed090a528a46681fab7
mmrsl618 <mmrsl618@github.com>
2026-06-21 07:37:58 +0800

    admin: publish

--- admin_server.py
+++ admin_server.py
@@ -141,16 +141,27 @@ def apply_master_template(content, title, desc, section='', sub_category=''):
         else:
             nav_html += f'      <a href="{url}">{lbl}</a>\n'
 
-    # 面包屑：Home › 板块名 › 文章标题（和文章详情页现有风格一致）
+    # 面包屑：Home › 板块名 › 子分类名
+    # 如果有子分类，显示子分类名；否则显示文章标题
     section_label = GUIDE_SECTIONS.get(section, section)
     section_url = f'../guides/{section}.html'
+    sub_label = ''
+    if sub_category:
+        for subs in SUBCAT_MAP.values():
+            for k, lbl in subs:
+                if k == sub_category:
+                    sub_label = lbl
+                    break
+            if sub_label:
+                break
+    last_crumb = sub_label if sub_label else title
     breadcrumb = (f'<div class="breadcrumb" style="font-size:0.85rem;color:#666;'
                   f'padding:0 0 1rem;margin:0 0 1rem;border-bottom:1px solid #eee;">\n'
                   f'<a href="../index.html">Home</a>\n'
                   f'<span class="sep"> › </span>\n'
                   f'<a href="{section_url}">{html_escape(section_label)}</a>\n'
                   f'<span class="sep"> › </span>\n'
-                  f'<span>{html_escape(title)}</span>\n</div>\n')
+                  f'<span>{html_escape(last_crumb)}</span>\n</div>\n')
 
     tpl = tpl.replace('__TITLE__', title)
     tpl = tpl.replace('__DESCRIPTION__', desc)
